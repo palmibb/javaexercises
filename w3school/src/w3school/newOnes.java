668a5c84fb3b7f2314ec043cd48a256adaffa65e
DESKTOP-VK2K1Q1\Carlos <cpalmi0725@gmail.com>
2023-02-17 18:29:22 -0500

    update de nuevos ejercicios

---
+++ w3school/src/w3school/newOnes.java
@@ -0,0 +1,24 @@
+package w3school;
+import java.util.Scanner;
+
+
+public class newOnes {
+    
+    public static void main(String[] args) {
+//FIRST ONE
+//    Scanner sd=new Scanner(System.in);
+//    int y;
+//    System.out.println("ingrese valor: ");
+//    y=sd.nextInt();
+//    for (int i=y; i<5; i++){
+//        System.out.println("Yes");
+//        }
+//    
+//SECOND ONE
+      String[] cars = {"Volvo", "BMW", "Mazda"};
+      for (String i : cars){
+          System.out.println(i);
+      }
+    }
+    
+}
